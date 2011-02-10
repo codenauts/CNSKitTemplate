3f54bc61be901c0284d3739ddf44a78b68109f55
Thomas Dohmke <thomas@dohmke.de>
2011-02-10 11:47:29 +0100

    Initial revision

---
+++ Classes/CNSApplicationDelegate.m
@@ -0,0 +1,130 @@
+// Copyright 2011 Codenauts UG. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "CNSApplicationDelegate.h"
+
+@interface CNSApplicationDelegate ()
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@end
+
+@implementation CNSApplicationDelegate
+
+#pragma mark -
+#pragma mark Application Delegate Methods
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+#ifdef ADHOC
+#warning Build includes Hockey framework
+  [[BWHockeyController sharedHockeyController] setBetaURL:@"URL" delegate:self];
+#endif
+  
+  [self.window makeKeyAndVisible];
+  
+  return YES;
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+  [self saveContext];
+}
+
+#pragma mark -
+#pragma mark Core Data Stack
+
+- (NSManagedObjectContext *)managedObjectContext {
+	if (managedObjectContext != nil) {
+		return managedObjectContext;
+	}
+	
+	NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+	if (coordinator != nil) {
+		managedObjectContext = [[NSManagedObjectContext alloc] init];
+		[managedObjectContext setPersistentStoreCoordinator:coordinator];
+    managedObjectContext.undoManager = [[[NSUndoManager alloc] init] autorelease];
+    managedObjectContext.undoManager.levelsOfUndo = 10;
+	}
+	return managedObjectContext;
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+	if (managedObjectModel != nil) {
+		return managedObjectModel;
+	}
+	NSString *modelPath = [[NSBundle mainBundle] pathForResource:@"Base" ofType:@"momd"];
+	NSURL *modelURL = [NSURL fileURLWithPath:modelPath];
+	managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];    
+	return managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+	if (persistentStoreCoordinator != nil) {
+		return persistentStoreCoordinator;
+	}
+	
+	NSURL *storeURL = [NSURL fileURLWithPath:[[self applicationDocumentsDirectory] stringByAppendingPathComponent: @"Base.sqlite"]];
+	
+  NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
+                           [NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption,
+                           [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+  
+	NSError *error = nil;
+	persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+	if (![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:options error:&error]) {
+		NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+		abort();
+	}    
+	
+	return persistentStoreCoordinator;
+}
+
+#pragma mark -
+#pragma mark Helper Methods
+
+- (NSString *)applicationDocumentsDirectory {
+	return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
+- (void)saveContext {
+  NSError *error = nil;
+  if (managedObjectContext != nil) {
+    if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
+      NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+      abort();
+    } 
+  }
+}    
+
+#pragma mark -
+#pragma mark Memory Managements Methods
+
+- (void)dealloc {
+  self.managedObjectContext = nil;
+  self.managedObjectModel = nil;
+  self.persistentStoreCoordinator = nil;
+  self.window = nil;
+  
+  [super dealloc];
+}
+
+@end
+
